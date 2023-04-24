7cb3dafe374b14d3dfb9b1612d561f572a4ac94c
João Bisneto <joaofeitoza.13@gmail.com>
2023-04-24 15:40:33 -0300

    feat: add restaurant basic form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,62 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+interface Restaurant {
+  name?: string,
+  address?: string,
+  status?: RestaurantStatus,
+  dishes?: Dish[]
+}
+
+type Dish = ''
+
+type RestaurantStatus = 
+  'Not Recommended' |
+  'Want to Try' |
+  'Recommended' |
+  'Must Try'
+
+const statusList = ['Do not Recommended', 'Want to Try', 'Recommended', 'Must Try']
+
+const restaurantList = ref<Restaurant[]>([])
+const newRestaurant = ref<Restaurant>({})
+
+function addRestaurant() {
+  restaurantList.value.push({
+    name: newRestaurant.value.name,
+    address: '',
+    status: newRestaurant.value.status,
+    dishes: []
+  })
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <pre> {{ newRestaurant }} </pre>
+    <form @submit.prevent="addRestaurant">
+      <div>
+        <label for="restaurant-name"> Restaurant Name </label>
+        <input id="restaurant-name" v-model="newRestaurant.name" type="text">
+      </div>
+      <div>
+        <label for="restaurant-address"> Restaurant Address</label>
+        <input id="restaurant-address" v-model="newRestaurant.address" type="text">
+      </div>
+      <div>
+        <label for="restaurant-status"> Restaurant Status</label>
+        <select name="restaurant-status" v-model="newRestaurant.status" id="restaurant-status">
+          <option v-for="status in statusList" :value="status" :key="status">
+            {{ status }}
+          </option>
+        </select>
+      </div>
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+      <li v-for="restaurant in restaurantList" :key="restaurant.name">
+        {{ restaurant.name }}
+      </li>
+    </ul>
   </main>
 </template>
